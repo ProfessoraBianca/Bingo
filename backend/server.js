5540163821749861bbbb0b2c0d6f6df3f9c5dd30
Professora Bianca <92760317+ProfessoraBianca@users.noreply.github.com>
2026-01-02 15:34:19 -0300

    Initialize Express server with CORS and routes

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const initCells = require('./initCells');
+const cellsRouter = require('./routes/cells');
+
+const app = express();
+
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(express.json());
+
+app.use('/cells', cellsRouter);
+
+connectDB().then(() => initCells());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
